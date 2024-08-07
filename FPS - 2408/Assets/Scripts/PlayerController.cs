3b8f706c747f52c644d4a8eda910a12f1113a34d
Ksokgirl <148779279+Ksokgirl@users.noreply.github.com>
2024-08-07 13:19:05 -0500

    Created serialized fields for shooting mecanics, included raycast for feedback on what is being hit, and added layerMask to prevent player from shooting themselves

--- FPS - 2408/Assets/Scripts/PlayerController.cs
+++ FPS - 2408/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
+    [SerializeField] private LayerMask ignoreMask;
     [SerializeField] private int speed;
     [SerializeField] private int sprintMod;
     [SerializeField] private int jumpMax;
     [SerializeField] private int jumpSpeed;
     [SerializeField] private int gravity;
 
+    [SerializeField] private int shootDamage;
+    [SerializeField] private float shootRate;
+    [SerializeField] private int shootDist;
+    
     private Vector3 move;
     private Vector3 playerVelocity;
 
     private int jumpCount;
 
     private bool isSprinting;
+    private bool isShooting;
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +57,9 @@ public class PlayerController : MonoBehaviour
 
         controller.Move(playerVelocity * Time.deltaTime);
         playerVelocity.y -= gravity * Time.deltaTime;
+
+        if(Input.GetButton("Shoot") && !isShooting)
+             StartCoroutine(shoot());
     }
 
     void Sprint()
@@ -65,5 +74,17 @@ public class PlayerController : MonoBehaviour
             speed /= sprintMod;
             isSprinting = false;
         }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, ~ignoreMask))
+        {
+            Debug.Log(hit.collider.name);
+        }
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
     }
 }
