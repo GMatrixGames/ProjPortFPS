421517e45f89aea5f0a78a6d0df4c776fda106ec
GMatrixGames <gmatrixgames@hotmail.com>
2024-08-06 22:41:08 -0400

    Implement Jumping

--- FPS - 2408/Assets/Scripts/PlayerController.cs
+++ FPS - 2408/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,17 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private int speed;
+    [SerializeField] private int sprintMod;
+    [SerializeField] private int jumpMax;
+    [SerializeField] private int jumpSpeed;
+    [SerializeField] private int gravity;
 
     private Vector3 move;
+    private Vector3 playerVelocity;
+
+    private int jumpCount;
+
+    private bool isSprinting;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +25,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVelocity = Vector3.zero;
+        }
+        
+        // move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         // transform.position += move * speed * Time.deltaTime;
 
+        move = Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right;
         controller.Move(move * (speed * Time.deltaTime));
+        
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVelocity.y = jumpSpeed;
+        }
+        
+        controller.Move(playerVelocity * Time.deltaTime);
+        playerVelocity.y -= gravity * Time.deltaTime;
     }
 }
