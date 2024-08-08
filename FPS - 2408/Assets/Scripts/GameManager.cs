d3d042e515e2ee0ee898675df94a32a01a2a8a3e
GMatrixGames <gmatrixgames@hotmail.com>
2024-08-08 19:31:23 -0400

    Pause menu states in GameManager

---
+++ FPS - 2408/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField] private GameObject menuActive;
+    [SerializeField] private GameObject menuPause;
+
+    public bool isPaused;
+
+    // GK: Custom timeScale, should be 1 by default.
+    [SerializeField] private int timeScale = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (!menuActive)
+            {
+                StatePause();
+                menuActive = menuPause;
+                menuActive.SetActive(isPaused);
+            }
+            else if (menuActive == menuPause)
+            {
+                StateUnpause();
+            }
+        }
+    }
+
+    public void StatePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None; // GK: Personally dislike exclusive windowed, so it's None.
+    }
+
+    public void StateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScale; // GK: Utilize stored timescale.
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+}
