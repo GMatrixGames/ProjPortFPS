4a6a81993b3fe7edb5365c76e2d0d928c66bb5b6
GMatrixGames <gmatrixgames@hotmail.com>
2024-08-09 00:28:39 -0400

    Add bullet, resolve double damage bug

--- FPS - 2408/Assets/Scripts/DamageTrigger.cs
+++ FPS - 2408/Assets/Scripts/DamageTrigger.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 
 public class DamageTrigger : MonoBehaviour
 {
-    [SerializeField] private int damageAmount;
+    private enum DamageType
+    {
+        Bullet,
+        Stationary
+    }
+
+    [SerializeField] private DamageType type;
+    [SerializeField] private Rigidbody rb;
+
+    [SerializeField] private int damage;
+    [SerializeField] private int speed;
+    [SerializeField] private int destroyTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (type == DamageType.Bullet)
+        {
+            rb.velocity = transform.forward * speed;
+            Destroy(gameObject, destroyTime);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -17,6 +33,8 @@ public class DamageTrigger : MonoBehaviour
         if (other.isTrigger) return;
 
         var dmg = other.GetComponent<IDamage>();
-        dmg?.TakeDamage(damageAmount);
+        dmg?.TakeDamage(damage);
+        
+        Destroy(gameObject);
     }
 }
