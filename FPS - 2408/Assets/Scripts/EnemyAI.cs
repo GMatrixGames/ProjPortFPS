9e4189db02af79869fb4a335bb52bdce1f7a28cf
Ksokgirl <148779279+Ksokgirl@users.noreply.github.com>
2024-08-09 05:43:29 -0500

    Serialized field for shooting position and adjusted bullet color

--- FPS - 2408/Assets/Scripts/EnemyAI.cs
+++ FPS - 2408/Assets/Scripts/EnemyAI.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour, IDamage
 {
     [SerializeField] private Renderer model;
+    [SerializeField] private Transform shootPos;
+
     [SerializeField] private int HP;
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float shootRate;
+
+    private bool isShooting;
 
     private Color colorOriginal;
 
@@ -18,6 +25,8 @@ public class EnemyAI : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
+        if (!isShooting)
+            StartCoroutine(shoot());
     }
 
     public void TakeDamage(int amount)
@@ -38,5 +47,13 @@ public class EnemyAI : MonoBehaviour, IDamage
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         model.material.color = colorOriginal;
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
     }
 }
