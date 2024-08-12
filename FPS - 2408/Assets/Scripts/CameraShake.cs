bd8ee8145942d616e0eff6370398c419ca6445e4
Chanel Maughmer <csmaughmer@student.fullsail.edu>
2024-08-11 23:47:43 -0500

    Retry to push

--- FPS - 2408/Assets/Scripts/CameraShake.cs
+++ FPS - 2408/Assets/Scripts/CameraShake.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Coroutine to shake the camera at a specified duration and magnitude
+// Duration = How long the shake should last
+// Magnitude = The intesity of the shake
+
 public class CameraShake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public IEnumerator Shake(float duration, float magnitude)
     {
-        
-    }
+        // Stores the original position of the camera
+        Vector3 origPos = transform.localPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float elasped = 0.0f;
+
+        // This is the loop for the duration of the shake effect
+        while(elasped < duration)
+        {
+            // This will give a random offset for the shake effect
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            // Applies the offset to the camera's position
+            transform.localPosition = new Vector3(x, y, origPos.z);
+
+            // Increments the elasped time
+            elasped += Time.deltaTime;
+
+            // Wait for next frame before continuing loop
+            yield return null;
+        }
+
+        // Resets the camera back to the original position after the effect
+        transform.localPosition = origPos; 
     }
 }
