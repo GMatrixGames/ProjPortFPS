80157b1d5ad2bacc16a10b323ae815000dce1507
MKMccrory <132397910+MKMccrory@users.noreply.github.com>
2024-08-12 18:03:45 -0400

    Made Changes
    Did a thing

---
+++ FPS - 2408/Assets/Scripts/RingBell.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingBell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Update();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
