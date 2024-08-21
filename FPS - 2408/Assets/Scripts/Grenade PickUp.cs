dee8adb859014694bbee36ab80a01a882bfb6c7f
Ksokgirl <148779279+Ksokgirl@users.noreply.github.com>
2024-08-20 23:02:50 -0500

    Made adjustment to GrenadePickUp Script and added grenade model

--- FPS - 2408/Assets/Scripts/Grenade PickUp.cs
+++ FPS - 2408/Assets/Scripts/Grenade PickUp.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GrenadePickUp : MonoBehaviour
 {
     public GameObject GrenadeOnPlayer;
-    public GameObject PickUpText;
+    public TMP_Text PickUpText;
     // Start is called before the first frame update
     void Start()
     {
         // Initially hide the grenade on the player
-        GrenadeOnPlayer.SetActive(false); 
-        PickUpText.SetActive(false);
+        GrenadeOnPlayer.SetActive(false);
+        PickUpText.enabled = false;
     }
 
     public void OnTriggerStay(Collider other) // OnTriggerStay is called continuously while the collider of this object is touching another object
@@ -19,7 +20,7 @@ public class GrenadePickUp : MonoBehaviour
         // Check if the other object is tagged as "Player"
         if (other.gameObject.tag == "Player")
         {
-            PickUpText.SetActive(true);
+            PickUpText.enabled = true;
 
             // Check if the player is pressing the 'E' key
             if (Input.GetKey(KeyCode.E))
@@ -30,7 +31,7 @@ public class GrenadePickUp : MonoBehaviour
                 // Show the grenade on the player
                 GrenadeOnPlayer.SetActive(true);
 
-                PickUpText.SetActive(false);
+                PickUpText.enabled = false;
             }
         }    
     }
@@ -38,6 +39,9 @@ public class GrenadePickUp : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         //removes PickUp text from screen 
-        PickUpText?.SetActive(false);
+        if (PickUpText != null)
+        {
+            PickUpText.enabled = false;
+        }
     }
 }
