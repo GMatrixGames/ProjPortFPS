2b43206d5d8569853f1dffb95283731e1300841f
Deferonz <155961879+Deferonz@users.noreply.github.com>
2024-09-26 11:38:12 -0400

    Grapple Range stuff.
    It's incomplete.

---
+++ FPS - 2408/Assets/Scripts/GrappleRangeIndicator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GrappleRangeIndicator : MonoBehaviour
+{
+    //Could potentially be used for something, but this seemed too much, so...
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("RunnableWall"))
+    //    {
+    //        other.GetComponent<Renderer>().material.color = Color.magenta;
+    //    }
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if(other.gameObject.CompareTag("RunnableWall"))
+    //    {
+    //        other.GetComponent<Renderer>().material.color = default;
+    //    }
+    //}
+}
